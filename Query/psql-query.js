9d2c5f7703cbaa6b041e2f65332c0cace154a243
preetbhingradiya <preetbhingradiya6@gmail.com>
2024-06-08 15:07:01 +0530

    name-filter

--- Query/psql-query.js
+++ Query/psql-query.js
@@ -3,9 +3,10 @@ const GET_ALL_TODOS = 'SELECT * FROM todo'
 const ADD_TODO = 'INSERT INTO todo (todo, date) VALUES ($1 , $2) RETURNING *'
 // const ADD_TODO = 'INSERT INTO todo (todo, date) VALUES ($1 , $2) '
 
-
 const SAME_DATE_TODO = `SELECT date from todo where date = $1`; //only return date value
 // const SAME_DATE_TODO = `SELECT * from todo where date = $1`; // return all the data properties
+
+const SAME_TODO_NAME = `SELECT * FROM todo WHERE todo LIKE $1`
 
 function FILTER_DATE(date) {
     return `SELECT * FROM todo WHERE date = '${date}'`
@@ -31,4 +32,4 @@ function DESC_NAME(limit) {
     return `SELECT * FROM todo  ORDER BY todo DESC LIMIT '${limit}'`
 }
 
-module.exports = { GET_ALL_TODOS, ADD_TODO, SAME_DATE_TODO, ASC_NAME, DESC_NAME, FILTER_DATE, EDIT_TODO, UPDATE_TODO, DELETE_TODO }
+module.exports = { GET_ALL_TODOS, ADD_TODO, SAME_DATE_TODO,SAME_TODO_NAME ,ASC_NAME, DESC_NAME, FILTER_DATE, EDIT_TODO, UPDATE_TODO, DELETE_TODO }
